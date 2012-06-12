b3cfc136ff89a0efa15b907ecafea9fc84a4db3f
Marc Zyngier <marc.zyngier@arm.com>
2012-06-12 13:30:55 +0100

    ARM: smp: Make smp_ops mandatory for SMP platforms
    Now that all SMP platforms have been converted to smp_ops, remove
    the "weak" attribute from the hooks in smp.c, and make the functions
    static wherever possible.
    
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

--- arch/arm/kernel/smp.c
+++ arch/arm/kernel/smp.c
@@ -131,25 +131,25 @@ void __init smp_ops_register(struct smp_ops *smp_ops)
 #endif
 }
 
-void __attribute__((weak)) __init smp_init_cpus(void)
+void __init smp_init_cpus(void)
 {
 	if (smp_init_ops && smp_init_ops->smp_init_cpus)
 		smp_init_ops->smp_init_cpus();
 }
 
-void __attribute__((weak)) __init platform_smp_prepare_cpus(unsigned int max_cpus)
+static void __init platform_smp_prepare_cpus(unsigned int max_cpus)
 {
 	if (smp_init_ops && smp_init_ops->smp_prepare_cpus)
 		smp_init_ops->smp_prepare_cpus(max_cpus);
 }
 
-void __attribute__((weak)) __cpuinit platform_secondary_init(unsigned int cpu)
+static void __cpuinit platform_secondary_init(unsigned int cpu)
 {
 	if (smp_secondary_ops && smp_secondary_ops->smp_secondary_init)
 		smp_secondary_ops->smp_secondary_init(cpu);
 }
 
-int __attribute__((weak)) __cpuinit boot_secondary(unsigned int cpu, struct task_struct *idle)
+int __cpuinit boot_secondary(unsigned int cpu, struct task_struct *idle)
 {
 	if (smp_secondary_ops && smp_secondary_ops->smp_boot_secondary)
 		return smp_secondary_ops->smp_boot_secondary(cpu, idle);
@@ -159,20 +159,20 @@ int __attribute__((weak)) __cpuinit boot_secondary(unsigned int cpu, struct task
 #ifdef CONFIG_HOTPLUG_CPU
 static void percpu_timer_stop(void);
 
-int __attribute__((weak)) platform_cpu_kill(unsigned int cpu)
+static int platform_cpu_kill(unsigned int cpu)
 {
 	if (smp_hotplug_ops && smp_hotplug_ops->cpu_kill)
 		return smp_hotplug_ops->cpu_kill(cpu);
 	return 0;
 }
 
-void __attribute__((weak)) platform_cpu_die(unsigned int cpu)
+static void platform_cpu_die(unsigned int cpu)
 {
 	if (smp_hotplug_ops && smp_hotplug_ops->cpu_die)
 		smp_hotplug_ops->cpu_die(cpu);
 }
 
-int __attribute__((weak)) platform_cpu_disable(unsigned int cpu)
+static int platform_cpu_disable(unsigned int cpu)
 {
 	if (smp_hotplug_ops && smp_hotplug_ops->cpu_disable)
 		return smp_hotplug_ops->cpu_disable(cpu);
@@ -182,7 +182,7 @@ int __attribute__((weak)) platform_cpu_disable(unsigned int cpu)
 /*
  * __cpu_disable runs on the processor to be shutdown.
  */
-int __cpu_disable(void)
+int __cpuinit __cpu_disable(void)
 {
 	unsigned int cpu = smp_processor_id();
 	int ret;
@@ -225,7 +225,7 @@ static DECLARE_COMPLETION(cpu_died);
  * called on the thread which is asking for a CPU to be shutdown -
  * waits until shutdown has completed, or it is timed out.
  */
-void __cpu_die(unsigned int cpu)
+void __cpuinit __cpu_die(unsigned int cpu)
 {
 	if (!wait_for_completion_timeout(&cpu_died, msecs_to_jiffies(5000))) {
 		pr_err("CPU%u: cpu didn't die\n", cpu);
