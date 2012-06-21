39ed367e47f8a5b7c06992a2ff3f49d56997be2a
Vitaly Andrianov <vitalya@ti.com>
2012-06-21 07:57:25 -0400

    ARM: add virt_to_idmap for interconnect aliasing
    On some PAE systems (e.g. TI Keystone), memory is above the 32-bit addressible
    limit, and the interconnect provides an aliased view of parts of physical
    memory in the 32-bit addressible space.  This alias is strictly for boot time
    usage, and is not otherwise usable because of coherency limitations.
    
    On such systems, the idmap mechanism needs to take this aliased mapping into
    account.  This patch introduces a virt_to_idmap() macro, which can be used on
    such sub-architectures to represent the interconnect supported boot time
    alias.  Most other systems would leave this macro untouched, i.e., do a simply
    virt_to_phys() and nothing more.
    
    Signed-off-by: Vitaly Andrianov <vitalya@ti.com>
    Signed-off-by: Cyril Chemparathy <cyril@ti.com>

--- arch/arm/kernel/smp.c
+++ arch/arm/kernel/smp.c
@@ -72,10 +72,10 @@ int __cpuinit __cpu_up(unsigned int cpu, struct task_struct *idle)
 	 */
 	secondary_data.stack = task_stack_page(idle) + THREAD_START_SP;
 
-	pgdir = virt_to_phys(idmap_pgd);
+	pgdir = virt_to_idmap(idmap_pgd);
 	secondary_data.pgdir = pgdir >> ARCH_PGD_SHIFT;
 
-	pgdir = virt_to_phys(swapper_pg_dir);
+	pgdir = virt_to_idmap(swapper_pg_dir);
 	secondary_data.swapper_pg_dir = pgdir >> ARCH_PGD_SHIFT;
 
 	__cpuc_flush_dcache_area(&secondary_data, sizeof(secondary_data));
